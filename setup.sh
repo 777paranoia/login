8a7b67a73a58d6dcbd7f3adb1fded654fd8e898c
777paranoia <68776519+777paranoia@users.noreply.github.com>
2024-11-05 11:37:59 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,14 @@ if ! command -v pip &> /dev/null; then
 fi
 
 mkdir -p ~/.login_log
-python3 -m venv ~/venv
-source ~/venv/bin/activate
-pip install -r requirements.txt
+cd ~/login-main
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install -r ./requirements.txt
 sed -i "s|%h|$HOME|g" com.showcrewnetwork.login.service
-sudo cp com.showcrewnetwork.login.service /etc/systemd/system/com.showcrewnetwork.login.service
-sudo cp com.showcrewnetwork.login.timer /etc/systemd/system/com.showcrewnetwork.login.timer
+sudo cp ./com.showcrewnetwork.login.service /etc/systemd/system/com.showcrewnetwork.login.service
+sudo cp ./com.showcrewnetwork.login.timer /etc/systemd/system/com.showcrewnetwork.login.timer
 sudo systemctl daemon-reload
-sudo systemctl enable com.showcrewnetwork.login.timer
-sudo systemctl start com.showcrewnetwork.login.timer
+sudo systemctl enable /etc/systemd/system/com.showcrewnetwork.login.timer
+sudo systemctl start /etc/systemd/system/com.showcrewnetwork.login.timer
+echo "booyah"
