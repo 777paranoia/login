a6eaf02f6fbf1b6d1d781e4a4c501bac5634da9b
777paranoia <68776519+777paranoia@users.noreply.github.com>
2024-11-05 12:36:10 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,22 +3,24 @@
 sudo apt update
 sudo apt install -y python3 python3-venv python3-pip chromium-driver
 
- if ! command -v pip &> /dev/null; then
-sudo apt install -y python3-pip
+if ! command -v pip &> /dev/null; then
+    sudo apt install -y python3-pip
 fi
 
 mkdir -p ~/.login_log
-touch ~/.login-log/login_script.log
+touch ~/.login_log/login_script.log
+
 cd ~/login-main
 python3 -m venv ./venv
 source ./venv/bin/activate
 pip install -r ./requirements.txt
+
 sed -i "s|%h|$HOME|g" com.showcrewnetwork.login.service
-sudo cp ./com.showcrewnetwork.login.service /etc/systemd/system
-com.showcrewnetwork.login.service
-sudo cp ./com.showcrewnetwork.login.timer /etc/systemd/system
-com.showcrewnetwork.login.timer
+
+sudo cp ./com.showcrewnetwork.login.service /etc/systemd/system/
+sudo cp ./com.showcrewnetwork.login.timer /etc/systemd/system/
+
 sudo systemctl daemon-reload
-sudo systemctl enable /etc/systemd/system/com.showcrewnetwork.login.timer
-sudo systemctl start /etc/systemd/system/com.showcrewnetwork.login.timer
-echo "booyah"
+sudo systemctl enable com.showcrewnetwork.login.timer
+sudo systemctl start com.showcrewnetwork.login.timer
+sudo systemctl status com.showcrewnetwork.login.timer
