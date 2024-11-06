b63c4dfe2237a4dbdd305f8d1afdcf4b1f78c925
777paranoia <68776519+777paranoia@users.noreply.github.com>
2024-11-06 00:52:03 -0600

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+sudo systemctl stop com.showcrewnetwork.login.timer
+sudo systemctl disable com.showcrewnetwork.login.timer
+sudo systemctl stop com.showcrewnetwork.login.service
+sudo systemctl disable com.showcrewnetwork.login.service
+sudo rm -f /etc/systemd/system/com.showcrewnetwork.login.service
+sudo rm -f /etc/systemd/system/com.showcrewnetwork.login.timer
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
+rm -rf ~/.login_log
+rm -r ~/login-main
